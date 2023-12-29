32fb2c4e76d85a4de5c7c812845f3711b8a5402d
Ana Beatriz <anabferreira0203@gmail.com>
2023-12-29 14:47:19 -0300

    Carrossel criado

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,51 +1,33 @@
-const carrosel = document.getElementById("carousel-example");
-const instanceOptions = {
-  id: "carousel-example",
-  override: true,
-};
+let carrosel = document.getElementById("carousel-example");
+const controls = document.querySelectorAll(".control");
+let currentItem = 0;
+const items = document.querySelectorAll(".item");
 
-const items = [
-  {
-    position: 0,
-    card01: document.getElementById("carousel-item-1"),
-  },
-  {
-    position: 1,
-    card02: document.getElementById("carousel-item-2"),
-  },
-  {
-    position: 2,
-    card03: document.getElementById("carousel-item-3"),
-  },
-];
-const options = {
-  defaultPosition: 1,
-  interval: 3000,
+const maxItems = items.length;
 
-  indicators: {
-    activeClasses: "bg-white dark:bg-gray-800",
-    inactiveClasses:
-      "bg-white/50 dark:bg-gray-800/50 hover:bg-white dark:hover:bg-gray-800",
-    items: [
-      {
-        position: 0,
-        card01: document.getElementById("carousel-item-1"),
-      },
-      {
-        position: 1,
-        card02: document.getElementById("carousel-item-2"),
-      },
-      {
-        position: 2,
-        card03: document.getElementById("carousel-item-3"),
-      },
-    ],
-  },
-};
-function onNext() {
-  console.log("next slider item is shown");
-}
+controls.forEach((controls) => {
+  controls.addEventListener("click", () => {
+    const isLeft = controls.classList.contains("arrow-left");
 
-function onPrev() {
-  console.log("next slider item is shown");
-}
+    if (isLeft) {
+      currentItem -= 1;
+    } else {
+      currentItem += 1;
+    }
+    if (currentItem >= maxItems) {
+      currentItem = 0;
+    }
+    if (currentItem < 0) {
+      currentItem = maxItems - 1;
+    }
+
+    items.forEach((item) => item.classList.remove("currente-item"));
+    console.log("controls clicked", isLeft, currentItem);
+
+    items[currentItem].scrollIntoView({
+      inline: "center",
+      behavior: "smooth",
+    });
+    items[currentItem].classList.add("currente-item");
+  });
+});
